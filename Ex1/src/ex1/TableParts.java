ca7ad58f77c025da02ba2a288e74f93154d9934c
caiqueaugustocm <bispo.caique@hotmail.com>
2018-12-05 09:09:20 -0300

    .
    .

--- Ex1/src/ex1/TableParts.java
+++ Ex1/src/ex1/TableParts.java
@@ -2,9 +2,7 @@
 package ex1;
 
 
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
 public class TableParts extends Rectangle{

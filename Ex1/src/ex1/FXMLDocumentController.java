7a7518f323125ef25428ecea7f342f5ed8cf7ad4
caiqueaugustocm <bispo.caique@hotmail.com>
2018-12-06 05:02:41 -0300

    FXMLDocumentController FINAL
    .

--- Ex1/src/ex1/FXMLDocumentController.java
+++ Ex1/src/ex1/FXMLDocumentController.java
@@ -14,6 +14,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
@@ -24,34 +26,53 @@ import javafx.stage.Stage;
  * @author Pedro Felipe
  */
 public class FXMLDocumentController implements Initializable {
-    @FXML TextField user1;
-    @FXML TextField user2;
+    @FXML private TextField txtuser1;
+    @FXML private TextField txtuser2;
 
     private Label label;
     Parent root;
     String nome1, nome2;
     
+    private boolean user(){
+        nome1 = "Maíra";
+        nome2 = "Marcus";      
+        nome1 = txtuser1.getText();
+        nome2 = txtuser2.getText();
+        System.out.println(nome1);
+        System.out.println(nome2);
+        return true;
+    }
+    
+    private void Message(){
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Player");
+        alert.setHeaderText("Enter the players names!");
+       
+    }
     @FXML
     private void handleButtonAction(ActionEvent event) {
+        
         try {
+            user();
+            if(nome1!=null || nome2!=null){
+                //System.out.println(nome1);
+                //System.out.println(nome2);
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Table.fxml"));
+                root = (Parent)fxmlLoader.load();
+                Stage stage = new Stage();
+                stage.initModality(Modality.APPLICATION_MODAL);
+                //stage.initStyle(StageStyle.UNDECORATED);           
+                stage.setTitle("Table");          
+                stage.setScene(new Scene(root, 50*20, 50*14));            
+                TableController controller = fxmlLoader.<TableController>getController();            
+                controller.setTable();
+                stage.show();
+            }
             
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Table.fxml"));
-            root = (Parent)fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.initModality(Modality.APPLICATION_MODAL);
-            //stage.initStyle(StageStyle.UNDECORATED);            
-            stage.setTitle("Table");
-            stage.setScene(new Scene(root, 50*20, 50*14));
             
-            TableController controller = fxmlLoader.<TableController>getController();
-            //controller.setPlayer(user1.getText(), user2.getText());
-            controller.setTable();
             
-            //controller.addElements();
             
-
-            stage.show();
-            // Esconde a janela atual
+            
             
                    
         }
@@ -59,6 +80,7 @@ public class FXMLDocumentController implements Initializable {
             e.printStackTrace();
             label.setText("Erro ao chamar a Table!" + e.getMessage());
         }
+        
     }
     
     
@@ -67,4 +89,6 @@ public class FXMLDocumentController implements Initializable {
         // TODO
     }    
 
+    
+    
 }
